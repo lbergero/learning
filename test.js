079bb9e0f0103f9181eea114ceb8db0246b02880
Luc Bergeron <luc.bergeron@gmail.com>
2018-03-10 11:26:46 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+var emitter = require('./myEmitter');
+
+var thisEmitter = new emitter(5);
+
+thisEmitter.on('start', function() {
+    console.log("starting");
+});
+
+thisEmitter.on('event', function(count) {
+    console.log('eventing ' + count);
+})
+
+thisEmitter.on('end', function() {
+    console.log('ending');
+});
+
+console.log("---");
